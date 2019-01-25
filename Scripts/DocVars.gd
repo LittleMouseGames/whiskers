55a70b7dc81adf1879d5edbde691119f43104c71
finchMFG <chris-1752@hotmail.com>
2019-01-24 22:06:42 -0500

    Update 'info' tab variables when they change!

--- Scripts/DocVars.gd
+++ Scripts/DocVars.gd
@@ -1,5 +1,7 @@
 extends VBoxContainer
 
+var isFocused = false
+
 func _ready():
 	for i in range(0, DemoSingleton.variables.size()):
 		var node = get_node("item_template").duplicate()
@@ -21,3 +23,16 @@ func _on_LineEdit_text_changed(new_text):
 			if(new_text == 'False') or (new_text == 'false'):
 				val = false
 			DemoSingleton.set(get_node(".").get_child(i).name, val)
+
+func _physics_process(delta):
+	if(!isFocused):
+		var items = get_node(".").get_child_count()
+		for i in range(1, items):
+			var nodeText = get_node(".").get_child(i).get_child(1).set_text(str(DemoSingleton.get(get_node(".").get_child(i).name)))
+		
+
+func _on_LineEdit_focus_entered():
+	isFocused = true
+
+func _on_LineEdit_focus_exited():
+	isFocused = false
