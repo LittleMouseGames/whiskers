dace7cbabd48e9123a7516f22000c79c88f8a863
finchMFG <chris-1752@hotmail.com>
2019-01-29 06:09:58 -0500

    :tada: You can now Drag a node onto the Graph!

---
+++ Scripts/DragHandle.gd
@@ -0,0 +1,9 @@
+extends ItemList
+
+# set data being dragged, in this case "good stuff"
+func get_drag_data(pos):
+	var selected = get_selected_items()
+	var prev = TextureRect.new()
+	prev.texture = get_item_icon(selected[0])
+	set_drag_preview(prev)
+	return get_item_text(selected[0])
