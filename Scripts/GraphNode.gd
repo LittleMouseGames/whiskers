91a3d0bca109117761bd35d3d1583d4be73981d9
finchMFG <chris-1752@hotmail.com>
2019-01-30 08:38:08 -0500

    :pen: Better naming conventions in GraphNode.gd

--- Scripts/GraphNode.gd
+++ Scripts/GraphNode.gd
@@ -1,17 +1,14 @@
 extends GraphNode
 
-func _on_Dialogue_close_request():
+func _on_Node_close_request():
 	self.queue_free()
 	print('removing node')
 
-func _on_Dialogue_resize_request(new_minsize):
+func _on_Node_resize_request(new_minsize):
 	self.rect_size = new_minsize
 
-func _on_Dialogue_dragged(from, to):
+func _on_Node_dragged(from, to):
 	get_node('../').lastNodePosition = to
 
-func _on_Option_dragged(from, to):
-	get_node('../').lastNodePosition = to
-
-func _on_Dialogue_resized():
+func _on_Node_resized():
 	get_node("Lines").rect_min_size.y = self.get_rect().size.y - 45
