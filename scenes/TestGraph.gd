de53a31a0a40cf465ac0ad6a0d7e822925251f7f
finchMFG <chris-1752@hotmail.com>
2019-11-09 12:40:19 -0500

    Better node styling

--- scenes/TestGraph.gd
+++ scenes/TestGraph.gd
@@ -1,7 +1,11 @@
 extends GraphEdit
 
 onready var test_node = load("res://config/nodes/start/node.tscn")
+onready var node_theme = self.get_theme()
 
 func _ready():
+	node_theme.set_constant("title_h_offset", "GraphNode", -5)
+	node_theme.set_constant("close_h_offset", "GraphNode", 10)
+	
 	test_node = test_node.instance()
 	self.add_child(test_node)
