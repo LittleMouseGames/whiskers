f3ca7207c557380fb249cf8062be97fb0a8c1783
void <theprismaticvoid@gmail.com>
2019-12-29 15:27:44 -0600

    Reduce nesting

--- Scripts/DemoGame.gd
+++ Scripts/DemoGame.gd
@@ -6,23 +6,25 @@ var currentTab
 var buttonAct
 
 func _physics_process(_delta):
-	if(currentTab == 1):
-		if Input.is_action_pressed("player_right") or buttonAct == 'right':
-			player.set_global_position(Vector2(player.get_global_position().x + speed, player.get_global_position().y))
-		if Input.is_action_pressed("player_left") or buttonAct == 'left':
-			player.set_global_position(Vector2(player.get_global_position().x - speed, player.get_global_position().y))
-		if Input.is_action_pressed("player_up") or buttonAct == 'up':
-			player.set_global_position(Vector2(player.get_global_position().x, player.get_global_position().y - speed))
-		if Input.is_action_pressed("player_down") or buttonAct == 'down':
-			player.set_global_position(Vector2(player.get_global_position().x, player.get_global_position().y + speed))
-		if Input.is_action_pressed("player_action") or buttonAct == 'action':
-			if(get_node("PlayField/Player/E").is_visible()):
-				# we should hide the movement buttons and our action button
-				get_node("Keys").hide()
-				get_node("PlayField/Player/E").hide()
-				# we should show our Dialogue Window!
-				get_node("Dialogue").show()
-				get_node("Dialogue").init()
+	if(currentTab != 1):
+		return
+	
+	if Input.is_action_pressed("player_right") or buttonAct == 'right':
+		player.set_global_position(Vector2(player.get_global_position().x + speed, player.get_global_position().y))
+	if Input.is_action_pressed("player_left") or buttonAct == 'left':
+		player.set_global_position(Vector2(player.get_global_position().x - speed, player.get_global_position().y))
+	if Input.is_action_pressed("player_up") or buttonAct == 'up':
+		player.set_global_position(Vector2(player.get_global_position().x, player.get_global_position().y - speed))
+	if Input.is_action_pressed("player_down") or buttonAct == 'down':
+		player.set_global_position(Vector2(player.get_global_position().x, player.get_global_position().y + speed))
+	if Input.is_action_pressed("player_action") or buttonAct == 'action':
+		if(get_node("PlayField/Player/E").is_visible()):
+			# we should hide the movement buttons and our action button
+			get_node("Keys").hide()
+			get_node("PlayField/Player/E").hide()
+			# we should show our Dialogue Window!
+			get_node("Dialogue").show()
+			get_node("Dialogue").init()
 
 
 func _on_Area2D2_area_entered(_area):
