98bfcbc60b2167a9d046851375b8ebcca02de8e1
finchMFG <chris-1752@hotmail.com>
2020-05-13 19:57:53 -0400

    Reset name on empty

--- scripts/singletons/serializer.gd
+++ scripts/singletons/serializer.gd
@@ -32,8 +32,12 @@ func save_setting(text, setting: String, node_name: String) -> void:
 	# TODO: Clean up
 	if setting == 'Name':
 		var node = get_node('/root/Window').find_node('GraphEdit').get_node(node_name)
+		var tree_name = text
+		if !text:
+			text = 'Node Name'
+			tree_name = node_name
 		node.find_node('Label', true).set_text(text)
-		loader_singleton.update_name(node_name, text, node.get_meta('type'))
+		loader_singleton.update_name(node_name, tree_name, node.get_meta('type'))
 	
 	scene.nodes[node_name][setting]['value'] = text
 
